7338a582dab3149d798a14682f215f54cdc523bd
Rodrigo Russo <68329584+RodrigoGaluppo@users.noreply.github.com>
2020-10-14 21:00:06 +0100

    agora vai

--- Searcher.py
+++ Searcher.py
@@ -15,7 +15,7 @@ class DolarSearcher:
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_driver_path = os.enciron.get("CHROME_DRIVER_PATH")
+        chrome_driver_path = os.environ.get("CHROME_DRIVER_PATH")
 
         self.driver = webdriver.Chrome(executable_path=chrome_driver_path,options=chrome_options)
         self.wait = WebDriverWait(
