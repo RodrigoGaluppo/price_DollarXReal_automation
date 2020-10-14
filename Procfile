68b203030acf7ab030c94dd16c1978affb60e59e
Rodrigo Russo <68329584+RodrigoGaluppo@users.noreply.github.com>
2020-10-14 22:12:30 +0100

    safe

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python app.py free
+web: python app.py
--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from flask import Flask,jsonify,request
+from flask_cors import CORS
 from Searcher import DolarSearcher
+import os
 
 app = Flask(__name__)
+
+cors = CORS(app,resource={r"/*":{"origns":"*"}})
 
 #calling the searcher
 searcher = DolarSearcher()
 searcher.Start()
 
-# building the request 
 @app.route("/price/dolar",methods=['GET'])
 def get_dolar_price(): 
     return jsonify({"price":searcher.dolarPrice})
 
+def main():
+    PORT = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=PORT)
+
+"""
+# building the request 
+@app.route("/price/dolar",methods=['GET'])
+def get_dolar_price(): 
+    return jsonify({"price":searcher.dolarPrice})
+"""
+
 if __name__ == '__main__':
-    app.run()
+    main()
